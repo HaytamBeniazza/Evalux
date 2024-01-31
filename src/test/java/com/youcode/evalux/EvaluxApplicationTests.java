b4dbfb9a7eb4c376379203e41a24a40261bf86fb
Haytam Beniazza <beniazza0@gmail.com>
2024-01-31 10:26:37 +0100

    first Version

---
+++ src/test/java/com/youcode/evalux/EvaluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youcode.evalux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
