bf2a4ca0324f2ba3b85008aefbff2d4276f7bae8
MAROUANE <marwaneaitelhaj001@gmail.com>
2024-02-04 16:40:37 +0100

    Add review CRUD operations to ReviewController and update reviews.html

--- src/main/java/com/example/demo/controller/ReviewController.java
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Review;
 import com.example.demo.services.interfaces.ReviewService;
 import lombok.AllArgsConstructor;
+
+import java.util.UUID;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 @AllArgsConstructor
 public class ReviewController {
@@ -17,5 +22,20 @@ public class ReviewController {
         model.addAttribute("username", authentication.getName());
         model.addAttribute("role", authentication.getAuthorities().stream().findFirst().get().getAuthority());
         return "reviews";
+    }
+    @GetMapping("/addreview")
+    public  String add(Model model){
+        model.addAttribute("review" , new Review());
+        return "add";
+    }
+    @GetMapping("/editreview/{id}")
+    public  String edit(Model model, @PathVariable("id") UUID id){
+        model.addAttribute("review", reviewService.getReviewById(id));
+        return "edit";
+    }
+
+    @GetMapping("/deletereview/{id}")
+    public  String delete(Model model){
+        return "delete";
     }
 }
